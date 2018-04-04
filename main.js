afdc46b6afb385b9071eabceb7cfff0d3d4517ff
CashewRose <danieagnoletti@gmail.com>
2018-04-04 11:50:31 -0500

    Initial file push

---
+++ main.js
@@ -0,0 +1,48 @@
+const numbers = [];
+let count = 0;
+
+for (let i = 0; i < 100; i++) {
+    count += 1;
+    numbers.push(count);
+}
+console.log(numbers);
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 5 === 0 && numbers[i] % 7 === 0 ) {
+        console.log("ChickenMonkey!");
+    }
+    else if (numbers[i] % 5 === 0 || numbers[i] % 7 === 0 ) {
+        if (numbers[i] % 5 === 0) {
+            console.log(numbers[i]);
+            console.log("Chicken");
+        }
+        if (numbers[i] % 7 === 0) {
+            console.log(numbers[i]);
+            console.log("Monkey");
+        }
+    }
+}const numbers = [];
+let count = 0;
+
+for (let i = -1; i < 100; i++) {
+    count += 1;
+    numbers.push(count);
+}
+console.log(numbers);
+
+for (let i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 5 === 0 && numbers[i] % 7 === 0 ) {
+        console.log("ChickenMonkey!");
+    }
+    else if (numbers[i] % 5 === 0) {
+            console.log(numbers[i]);
+            console.log("Chicken");
+    }
+    else if (numbers[i] % 7 === 0) {
+            console.log(numbers[i]);
+            console.log("Monkey");
+    }
+    else {
+        console.log(numbers[i]);
+    }
+}
